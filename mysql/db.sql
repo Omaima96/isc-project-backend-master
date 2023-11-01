2d6baf220bb60977bceae1eaeaefccf5b6eba0d7
omaima96 <omaimam1396@gmail.com>
2023-11-01 15:39:31 +0100

    edits

--- mysql/db.sql
+++
@@ -1,4 +0,0 @@
-#ACCOUNT
-INSERT INTO `isc`.`user` (`id`, `email`, `phone_number`, `password`, `username`, `usertype`) VALUES ('1', 'leo@gmail.com', 5546738754, 'B123456@!', 'leo8961',"ADMIN");
-INSERT INTO `isc`.`user` (`id`, `email`, `phone_number`, `password`, `username`, `usertype`) VALUES ('2', 'paul@gmail.com', 3356789554, 'A123456@!', 'paul444',"USER");
-INSERT INTO `isc`.`user` (`id`, `email`, `phone_number`, `password`, `username`, `usertype`) VALUES ('3', 'ali@gmail.com', 3356542884, 'C123456@!', 'ali864',"SUPERADMIN");
